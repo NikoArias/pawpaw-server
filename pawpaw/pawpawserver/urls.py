09631657e637e3953f80758df00b23831ac6deb5
Nicolas Arias Lopez <190563@llinstitute.com>
2021-04-22 17:44:29 -0500

    Project Finished

--- pawpaw/pawpawserver/urls.py
+++ pawpaw/pawpawserver/urls.py
@@ -5,7 +5,9 @@ from pawpawserver import views
 urlpatterns = [
     path('', views.papaw_index),
     path('instruments', views.pawpaw_instrument),
+    path('instruments/<int:pk>/', views.instrument_detail),
     path('time-series-data', views.pawpaw_tsd),
+    path('time-series-data/<int:pk>/', views.tsd_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- pawpaw/pawpawserver/views.py
+++ pawpaw/pawpawserver/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from pawpawserver.models import TimeSeriesDatum, Instrument
 from pawpawserver.serializers import TimeSeriesDatumSerializer,InstrumentSerializer
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 
@@ -39,3 +40,49 @@ def pawpaw_instrument(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@csrf_exempt
+def instrument_detail(request, pk):
+    try:
+        instrument = Instrument.objects.get(pk=pk)
+    except Instrument.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = InstrumentSerializer(instrument)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = InstrumentSerializer(instrument, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        instrument.delete()
+        return HttpResponse(status=204)
+
+@csrf_exempt
+def tsd_detail(request, pk):
+    try:
+        instrument = Instrument.objects.get(pk=pk)
+    except Instrument.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = InstrumentSerializer(instrument)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = InstrumentSerializer(instrument, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        instrument.delete()
+        return HttpResponse(status=204)
